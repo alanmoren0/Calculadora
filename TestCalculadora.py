90696cb1601404e7afb048b6f0514911633a69c1
alanmoren0 <alan_moren0@hotmail.com>
2019-02-06 09:07:23 -0600

    Agregra metodo de 2_mas_4

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -5,6 +5,9 @@ class TestCalculadora(unitTest.TestCase):
 	def test_suma_2_mas_2(self):
 		calc=Calculadora()
 		self.asserrEquals(4,calc.suma(2,2))
+	def test_suma_2_mas_4(self):
+		calc=Calculadora()
+		self.asserrEquals(6,calc.suma(2,4))
 
 if __name__ =='__main__':
 	unittest.main()
