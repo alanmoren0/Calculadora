cd0a5f51f74322c5eef1bb521a1926ad8cc48f7e
alanmoren0 <alan_moren0@hotmail.com>
2019-02-25 08:58:59 -0600

    correcionde la correcion codigo

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -6,11 +6,11 @@ from Calculadora import Calculadora
 class TestCalculadora(unittest.TestCase):
     def test_suma_2_mas_2(self):
         calc = Calculadora()
-        self.asserrEquals(4, calc.suma(2, 2))
+        self.assertEquals(4, calc.suma(2, 2))
 
     def test_suma_2_mas_4(self):
         calc = Calculadora()
-        self.asserrEquals(6, calc.suma(2, 4))
+        self.assertEquals(6, calc.suma(2, 4))
 
 
 if __name__ == '__main__':
